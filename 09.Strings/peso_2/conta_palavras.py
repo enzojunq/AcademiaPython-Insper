0e425679db836eff9b0f801218e3d1e235efb312
Enzo <enzopj@al.insper.edu.br>
2023-05-08 15:03:35 -0300

    conta palavras com sorted de dict

---
+++ 09.Strings/peso_2/conta_palavras.py
@@ -0,0 +1,19 @@
+pontuacoes=['!','?','.',',']
+def conta_palavras(frase:str):
+    contagem={}
+    nova_frase=frase
+    for pontuacao in pontuacoes:
+        nova_frase=nova_frase.replace(pontuacao,'')
+    nova_frase=nova_frase.split(' ')
+    for palavra in nova_frase:
+        palavra=palavra.lower()
+        if palavra not in contagem:
+            contagem[palavra]=1
+        else:
+            contagem[palavra]+=1
+        ### SORTED DE DICT ###
+    contagem = dict(sorted(contagem.items(), key=lambda item: item[1], reverse=True))
+    return contagem
+
+string = "Qual caminho devo tomar? Eu não sei para onde ir! disse Alice. Se você não sabe onde quer ir, qualquer caminho serve. disse o Gato."
+print(conta_palavras(string))
